ab119fba73f105877687f806ab731682956a9991
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-10-28 20:40:27 +0900

    feat : 인원모집 & 미션 성공 게시판 생성

--- src/pages/find-partners/FindPartners.tsx
+++ src/pages/find-partners/FindPartners.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-import NavigationBar from "shared/ui/NavigationBar";
+import React from 'react';
+import NavigationBar from 'shared/ui/NavigationBar';
+import { useNavigate } from 'react-router-dom';
 
 const FindPartners = () => {
+  const navigate = useNavigate();
+  const handlePostSubmit = () => {
+    navigate('/create-partner-post');
+  };
   return (
     <div>
       <NavigationBar />
-      FindPartners
+      <div className="flex justify-center m-4 sm:m-10">
+        <div className="flex flex-col bg-custom_teal-300 p-8 sm:p-10 h-auto w-auto min-h-[400px] max-h-[800px] min-w-[600px] max-w-[800px] rounded-lg shadow-lg">
+          <div className="flex justify-between">
+            <h2 className="text-xl sm:text-2xl font-semibold mb-4">인원 모집</h2>
+            <div
+              className="bg-custom_teal-400 text-black font-semibold py-2 px-4 rounded-lg shadow hover:bg-white transition duration-200 cursor-pointer"
+              onClick={handlePostSubmit}
+            >
+              작성하기
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
