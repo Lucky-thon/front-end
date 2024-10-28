710c3d78d8d1a2daa6eb61c6a63edd6d92c08c1e
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-10-28 20:40:59 +0900

    feat : 인원모집 & 미션 성공 게시글 작성 페이지 생성

---
+++ src/pages/create-post/CreatePartnerPost.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavigationBar from 'shared/ui/NavigationBar';
+
+const CreatePartnerPost = () => {
+  return (
+    <div>
+      <NavigationBar />
+      CreatePartnerPost
+    </div>
+  );
+};
+
+export default CreatePartnerPost;
