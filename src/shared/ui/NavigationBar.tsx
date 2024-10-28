2bbbfd1b07fbea4199bfc059e61ab0bf6180f6c1
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-10-28 10:45:19 +0900

    design : fixed Background color and change

--- src/shared/ui/NavigationBar.tsx
+++ src/shared/ui/NavigationBar.tsx
@@ -1,26 +1,24 @@
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const NavigationBar = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
   const navItems = [
-    { label: "홈", path: "/" },
-    { label: "인원 모집", path: "/find-partners" },
-    { label: "미션 성공", path: "/success-album" },
-    { label: "사용 설명서", path: "/user-guide" },
+    { label: '홈', path: '/' },
+    { label: '인원 모집', path: '/find-partners' },
+    { label: '미션 성공', path: '/success-album' },
+    { label: '사용 설명서', path: '/user-guide' },
   ];
 
   return (
-    <div className="flex justify-around items-center px-4 py-3 sm:px-8 sm:py-6 bg-custom_teal-400 w-full">
+    <div className="flex justify-around items-center h-[100px] px-4 py-3 sm:px-8 sm:py-6 bg-custom_teal-300 w-full">
       {navItems.map((item, index) => (
         <span
           key={index}
           onClick={() => navigate(item.path)}
           className={`cursor-pointer ${
-            location.pathname === item.path
-              ? "text-black font-bold"
-              : "text-white"
+            location.pathname === item.path ? 'text-black font-bold' : 'text-white'
           } hover:text-pink-600 text-xs sm:text-sm md:text-base`}
         >
           {item.label}
@@ -29,10 +27,10 @@ const NavigationBar = () => {
       <img
         src="/assets/hallow_bell.svg"
         alt="hallow_bell_image"
-        onClick={() => navigate("/notification")}
+        onClick={() => navigate('/notification')}
         className={`cursor-pointer ${
-          location.pathname === "/notification" ? "text-red-500" : ""
-        } hover:opacity-80 h-5 w-5 sm:h-6 sm:w-6 md:h-8 md:w-8`}
+          location.pathname === '/notification' ? 'text-red-500' : ''
+        } hover:opacity-80 h-4 w-4 sm:h-5 sm:w-5 md:h-5 md:w-5`}
       />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   content: [],
   theme: {
     colors: {
+      background_elevated: '#E3FDFD',
       custom_teal: {
-        100: "#E3FDFD",
-        200: "#CBF1F5",
-        300: "#A6E3E9",
-        400: "#71C9CE",
+        200: '#CBF1F5',
+        300: '#A6E3E9',
+        400: '#71C9CE',
       },
     },
     extend: {},
