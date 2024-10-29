c2cce881cd2453ff3cbba00103d8a617d69a447b
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-10-29 16:43:07 +0900

    feat : initialize Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCiXPlT8MfQ_19NN-IOkDYqyYYYhhRc5A8',
+  authDomain: 'x-clone-81ee9.firebaseapp.com',
+  projectId: 'x-clone-81ee9',
+  storageBucket: 'x-clone-81ee9.appspot.com',
+  messagingSenderId: '817700290047',
+  appId: '1:817700290047:web:4af741ddc6247b3d133bca',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
