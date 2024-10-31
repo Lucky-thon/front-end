f48c1a0a43f56abdec56f82bd64c1809ea5f4888
silly0302 <kj0225123@gmail.com>
2024-10-31 21:31:02 +0900

    Update NavigationBar.tsx

--- src/shared/ui/NavigationBar.tsx
+++ src/shared/ui/NavigationBar.tsx
@@ -12,35 +12,39 @@ const NavigationBar = () => {
   ];
 
   return (
-    <div className="flex justify-around items-center h-[100px] px-4 py-3 sm:px-8 sm:py-6 bg-custom_teal-300 w-full">
-      <img src="/assets/service_logo.svg" alt="our_logo" />
-      {navItems.map((item, index) => (
-        <span
-          key={index}
-          onClick={() => navigate(item.path)}
+    <div className="flex justify-center items-center h-[100px] px-4 py-3 sm:px-8 sm:py-6 bg-custom_teal-300 w-full">
+      <div className="flex items-center space-x-20">
+        <img src="/assets/service_logo.svg" alt="our_logo" />
+        {navItems.map((item, index) => (
+          <span
+            key={index}
+            onClick={() => navigate(item.path)}
+            className={`cursor-pointer ${
+              location.pathname === item.path ? 'text-active_category font-bold' : 'text-black'
+            } hover:text-pink-600 text-xs sm:text-sm md:text-base`}
+          >
+            {item.label}
+          </span>
+        ))}
+      </div>
+      <div className="flex items-center ml-auto">
+        <img
+          src="/assets/hallow_bell.svg"
+          alt="hallow_bell_image"
+          onClick={() => navigate('/notification')}
           className={`cursor-pointer ${
-            location.pathname === item.path ? 'text-active_category font-bold' : 'text-black'
-          } hover:text-pink-600 text-xs sm:text-sm md:text-base`}
-        >
-          {item.label}
-        </span>
-      ))}
-      <img
-        src="/assets/hallow_bell.svg"
-        alt="hallow_bell_image"
-        onClick={() => navigate('/notification')}
-        className={`cursor-pointer ${
-          location.pathname === '/notification' ? 'text-red-500' : ''
-        } hover:opacity-80 h-4 w-4 sm:h-5 sm:w-5 md:h-5 md:w-5`}
-      />
-      
-      {/* 프로필 설정 이미지 추가 */}
-      <img
-        src="/assets/image.png" // 여기에 이미지 경로를 적어줘
-        alt="user_avatar"
-        onClick={() => navigate('/profile')} // ProfileSettingsPage.tsx로 넘어감
-        className="cursor-pointer w-12 h-12 rounded-full mr-80"  // 크기와 둥근 테두리 설정, 오른쪽 여백 추가
-      />
+            location.pathname === '/notification' ? 'text-red-500' : ''
+          } hover:opacity-80 h-5 w-5`}
+          style={{ marginRight: '70px' }}
+        />
+        <img
+          src="/assets/image.png" // 여기에 이미지 경로를 적어줘
+          alt="user_avatar"
+          onClick={() => navigate('/profile')} // ProfileSettingsPage.tsx로 넘어감
+          className="cursor-pointer w-12 h-12 rounded-full"
+          style={{ marginRight: '90px' }}
+        />
+      </div>
     </div>
   );
 };
