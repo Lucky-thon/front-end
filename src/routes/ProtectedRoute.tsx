68aadd9e2c7eeff89adcf57b7066e65a6c484679
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2024-10-31 16:41:37 +0900

    feat : 페이지 접근 제한
    로그인된 사용자만 서비스 페이지 접근 가능

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { Navigate } from 'react-router-dom';
+import React from 'react';
+
+interface ProtectedRouteProps {
+  element: React.ReactElement;
+  isAuthenticated: boolean;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element, isAuthenticated }) => {
+  return isAuthenticated ? element : <Navigate to="/signup" />;
+};
+
+export default ProtectedRoute;
